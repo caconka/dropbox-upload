00f5dfacc0e65342ff7ff998cbe4a88441ddd6a2
Carlos Cabezas <carloscabezascontra@gmail.com>
2018-02-17 21:15:55 +0100

    feat: middleware secure upload folder

--- app/post/post.routes.js
+++ app/post/post.routes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const controller = require('./post.controller');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
+const exist = require('../../middlewares/exist.middleware');
 
 router.get('/', controller.posts);
-router.post('/', upload.single('photo'), controller.createPost);
+router.post('/', exist.existsUploadsFolder, upload.single('photo'), controller.createPost);
 
 module.exports = router;
